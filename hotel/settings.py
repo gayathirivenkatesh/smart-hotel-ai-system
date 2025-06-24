39fab0fa3d7f675e21343af80b2a9f0c78b1f64c
GAYATHIRI V <166318038+gayathirivenkatesh@users.noreply.github.com>
2025-06-24 16:12:12 +0530

    Update settings.py

--- hotel/settings.py
+++ hotel/settings.py
@@ -24,9 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cafusabcdefghiacfdeysaczdsfoapqwsbbbbbbbbbbbsaaaaaaaaaaa' #this is the dummy key
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
